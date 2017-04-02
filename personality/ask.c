0075dc5943d8a78ef1fa000d449f10359b0bcd2b
Aliya Rahman <aliya.rahman@yahoo.com>
2017-04-02 12:42:47 -0400

    Added basic command line input collector

---
+++ personality/ask.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+     
+     char response[100];
+     int i;     
+     printf("\nRobot says: What's up?\nYou say: ");
+     scanf("%s", response);
+
+     printf("\n%s? Dope.\n\n", response);
+     return 0;
+
+     }
