76c86cb80c44be65f1aa16db4dc5f0559e9a6177
Aliya Rahman <aliya.rahman@yahoo.com>
2017-05-04 10:38:52 -0400

    Added image and xml files

--- eyes/facial_recognition.py
+++ eyes/facial_recognition.py
@@ -6,11 +6,11 @@ import cv2
 
 # Haar-cascade detection in OpenCV
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('intel_files/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('intel_files/haarcascade_eye.xml')
 
 
-img = cv2.imread('sachin.jpg')
+img = cv2.imread('aliya.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
